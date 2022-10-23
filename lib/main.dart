f0db21f28cbd049fc6dd861475a2c0d29aa86f37
kiminjoong <ijboym216@gmail.com>
2022-10-23 14:23:01 +0900

    1.  Injecting AuthRepository, StreamProvider, AuthProvider into widget t

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fb_auth_provider/pages/home_page.dart';
+import 'package:fb_auth_provider/pages/signin_page.dart';
+import 'package:fb_auth_provider/pages/signup_page.dart';
+import 'package:fb_auth_provider/pages/splash_page.dart';
+import 'package:fb_auth_provider/providers/auth_provider.dart';
+import 'package:fb_auth_provider/repositories/auth_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart' as fbAuth;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,21 +22,39 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        Provider<AuthRepository>(
+          create: (context) => AuthRepository(
+              firebaseFirestore: FirebaseFirestore.instance,
+              firebaseAuth: fbAuth.FirebaseAuth.instance),
+        ),
+        StreamProvider<fbAuth.User?>(
+            create: (context) => context.read<AuthRepository>().user,
+            initialData: null),
+        ChangeNotifierProxyProvider<fbAuth.User?, AuthProvider>(
+          create: (context) => AuthProvider(
+            authRepository: context.read<AuthRepository>(),
+          ),
+          update: (
+            BuildContext context,
+            fbAuth.User? userstream,
+            AuthProvider? authProvider,
+          ) =>
+              authProvider!..update(userstream),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Auth Provider',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: SplashPage(),
+        routes: {
+          SignupPage.routeName: (context) => SignupPage(),
+          SigninPage.routeName: (context) => SigninPage(),
+          HomePage.routeName: (context) => HomePage(),
+        },
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
