fd77afdfa8259a6726616a9501a36295b68b751e
kiminjoong <ijboym216@gmail.com>
2022-10-23 20:35:11 +0900

    1. SigninState, SigninProvider ChangeNotifier

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:fb_auth_provider/pages/home_page.dart';
 import 'package:fb_auth_provider/pages/signin_page.dart';
 import 'package:fb_auth_provider/pages/signup_page.dart';
 import 'package:fb_auth_provider/pages/splash_page.dart';
-import 'package:fb_auth_provider/providers/auth_provider.dart';
+import 'package:fb_auth_provider/providers/auth/auth_provider.dart';
+import 'package:fb_auth_provider/providers/signin/signin_provider.dart';
 import 'package:fb_auth_provider/repositories/auth_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart' as fbAuth;
 import 'package:firebase_core/firebase_core.dart';
@@ -42,6 +43,11 @@ class MyApp extends StatelessWidget {
             AuthProvider? authProvider,
           ) =>
               authProvider!..update(userstream),
+        ),
+        ChangeNotifierProvider<SigninProvider>(
+          create: (context) => SigninProvider(
+            authRepository: context.read<AuthRepository>(),
+          ),
         )
       ],
       child: MaterialApp(
--- lib/providers/auth/auth_provider.dart
+++ lib/providers/auth/auth_provider.dart
@@ -1,4 +1,4 @@
-import 'package:fb_auth_provider/providers/auth_state.dart';
+import 'package:fb_auth_provider/providers/auth/auth_state.dart';
 import 'package:fb_auth_provider/repositories/auth_repository.dart';
 import 'package:flutter/foundation.dart';
 import 'package:firebase_auth/firebase_auth.dart' as fbAuth;
