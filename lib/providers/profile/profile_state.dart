de438d22bddae85f8d2b8776a174c77055464049
kiminjoong <ijboym216@gmail.com>
2022-11-15 10:50:55 +0900

    1. ProfileRepository, ProfileState, ProfileProvider ChangeNotifier

---
+++ lib/providers/profile/profile_state.dart
@@ -0,0 +1,45 @@
+import 'package:fb_auth_provider/models/custom_error.dart';
+import 'package:fb_auth_provider/models/user_model.dart';
+
+enum ProfileStatus {
+  initial,
+  loading,
+  loaded,
+  error,
+}
+
+class ProfileState {
+  final ProfileStatus profileStatus;
+  final User user;
+  final CustomError error;
+
+  ProfileState({
+    required this.profileStatus,
+    required this.user,
+    required this.error,
+  });
+
+  factory ProfileState.initial() {
+    return ProfileState(
+      profileStatus: ProfileStatus.initial,
+      user: User.initialUser(),
+      error: CustomError(),
+    );
+  }
+
+  List<Object> get props => [profileStatus, user, error];
+
+  bool get stringify => true;
+
+  ProfileState copyWith({
+    ProfileStatus? profileStatus,
+    User? user,
+    CustomError? error,
+  }) {
+    return ProfileState(
+      profileStatus: profileStatus ?? this.profileStatus,
+      user: user ?? this.user,
+      error: error ?? this.error,
+    );
+  }
+}
