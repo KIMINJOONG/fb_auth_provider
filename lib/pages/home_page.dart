5c083ace7792664a35c695f5078abe3c14936112
kiminjoong <ijboym216@gmail.com>
2022-11-15 10:34:14 +0900

    1. Chapter 93. HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
+import 'package:fb_auth_provider/pages/profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../repositories/auth_repository.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,9 +19,62 @@ class _HomePageState extends State<HomePage> {
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Text('Home'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                context.read<AuthRepository>().signout();
+              },
+              icon: Icon(
+                Icons.exit_to_app,
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ProfilePage();
+                    },
+                  ),
+                );
+              },
+              icon: Icon(
+                Icons.account_circle,
+              ),
+            ),
+          ],
+        ),
         body: Center(
-          child: Text(
-            'home',
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/flutter_favorite.png',
+                    width: 100,
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  Text(
+                    'Provider',
+                    style: TextStyle(
+                      fontSize: 42,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+            ],
           ),
         ),
       ),
